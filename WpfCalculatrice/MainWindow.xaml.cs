c5572bfe189264b6515698369f135c1d95ff9f22
Moulinmar <marcus.moulin@rpn.ch>
2017-11-17 09:21:09 +0100

    Create a basic Calculatrice in WPF

---
+++ WpfCalculatrice/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCalculatrice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Addition addition;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Addition addition = new Addition(33, 5);
+        }
+
+        private void tbxNombre2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            addition.setVal1(22);
+            addition.setVal2(33);
+            lblReponse.Content = addition.Additionne();
+            MessageBox.Show(addition.Affiche());
+        }
+    }
+}
